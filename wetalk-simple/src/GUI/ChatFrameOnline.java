ffa348e138424f1a564b2fda5b54f2470b77f46a
LY-LongYi <wataxiwalongyi@163.com>
2015-07-20 17:29:58 +0800

    Revert "添加闪烁接口"
    This reverts commit 39c674bc19a1a9bfcb32fda98cfc192f527968a4.

--- wetalk-simple/src/GUI/ChatFrameOnline.java
+++ wetalk-simple/src/GUI/ChatFrameOnline.java
@@ -121,7 +121,8 @@ public class ChatFrameOnline extends JFrame {
 				//用户上线，显示主窗口
 				this.setVisible(false);		
 			
-				WeTalkDownPart.Start(comeQueue, goQueue, me);
+				WeTalkDownPart down = new WeTalkDownPart(comeQueue, goQueue, me);
+				down.start();
 			
 				log.info("show main frame.");
 				ChatFrameMain.showFrameMain(comeQueue, goQueue,me);
